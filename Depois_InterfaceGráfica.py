19692ea7a5e5c55e09943674b89cb919d8b44c60
FelipeAdr05 <felipeadrnunes@gmail.com>
2022-06-06 23:43:11 -0300

    Continuação da prática em Interface Gráfica

---
+++ Depois_InterfaceGráfica.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+sg.theme('SystemDefaultForReal')
+
+layout = [
+    [sg.Image(filename='entra.png')],
+    [sg.Text('Usuário', size=(7, 1)), sg.Input()],
+    [sg.Text('Senha', size=(7, 1)), sg.Input()],
+    [sg.Checkbox('Salvar Login?')],
+    [sg.Text('Esqueceu a senha? Clique Aqui')],
+    [sg.Button('Entrar'), sg.Button('Cadastrar')],
+
+]
+
+janela = sg.Window('Login', layout=layout, element_justification='c')
+janela.read()
+janela.close()
