b26db8638003d0fa0329c1f22f117aacd7cda676
FelipeAdr05 <felipeadrnunes@gmail.com>
2022-06-25 16:20:23 -0300

    Update Bot_login_hotmail.py

--- Automação/Selenium/Bot_login_hotmail.py
+++ Automação/Selenium/Bot_login_hotmail.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-navegador = webdriver.Chrome()
 
+email = str(input('Digite seu email: '))
+senha = str(input('Digite sua senha: '))
+
+navegador = webdriver.Chrome()
 navegador.get("https://login.live.com/")
-navegador.find_element(By.XPATH, '//*[@id="i0116"]').send_keys("email")
+navegador.find_element(By.XPATH, '//*[@id="i0116"]').send_keys(email)
 navegador.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
 sleep(0.8)
-navegador.find_element(By.XPATH, '//*[@id="i0118"]').send_keys("senha")
+navegador.find_element(By.XPATH, '//*[@id="i0118"]').send_keys(senha)
 navegador.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
 navegador.find_element(By.XPATH, '//*[@id="idBtn_Back"]').click()
