86205e8ca35fa36201f06ed0b8b29f723d5d15c2
Felipe Adriel Nunes <felipeadrnunes@gmail.com>
2022-07-01 11:28:13 -0300

    Update ex002.py

--- Orientação a Objetos/Exercícios/ex002.py
+++ Orientação a Objetos/Exercícios/ex002.py
@@ -3,17 +3,17 @@ class Quadrado:
         self.tamanhodolado = tamanhodolado
 
     def mudarvalor(self, novo_tamanho):
-            print(f'O tamanho do quadrado era: {self.tamanhodolado}, e virou {novo_tamanho}')
+            print(f'O tamanho do quadrado era: {self.tamanhodolado}cm, e virou {novo_tamanho}cm')
             self.tamanhodolado = int(novo_tamanho)
 
     def calcular(self):
         area = self.tamanhodolado**2
-        print(f'A área do quadrado é {area}')
+        print(f'A área do quadrado é {area}cm²')
 
 quadrado = Quadrado(10)
 
 while True:
-    print(f'O tamanho do quadrado é {self.tamanhodolado}')
+    print(f'O tamanho do quadrado é {quadrado.tamanhodolado}cm')
     quadrado.mudarvalor(novo_tamanho=input('Qual o novo tamanho desejado? '))
     r = input(f'Deseja calcular a área deste quadrado? ').upper()
     if r == 'S':
